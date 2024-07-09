12b8766a052f7676866a88536ddd35c66f745ffc
Alex Martin <sharkfinpro@hotmail.com>
2024-07-09 11:32:09 -0700

    Update VulkanApp.cpp

--- source/VulkanApp.cpp
+++ source/VulkanApp.cpp
@@ -1579,11 +1579,8 @@ VkFormat VulkanApp::findSupportedFormat(const std::vector<VkFormat>& candidates,
     VkFormatProperties props;
     vkGetPhysicalDeviceFormatProperties(physicalDevice, format, &props);
 
-    if (tiling == VK_IMAGE_TILING_LINEAR && (props.linearTilingFeatures & features) == features)
-    {
-      return format;
-    }
-    else if (tiling == VK_IMAGE_TILING_OPTIMAL && (props.optimalTilingFeatures & features) == features)
+    if ((tiling == VK_IMAGE_TILING_LINEAR && (props.linearTilingFeatures & features) == features) ||
+        (tiling == VK_IMAGE_TILING_OPTIMAL && (props.optimalTilingFeatures & features) == features))
     {
       return format;
     }
