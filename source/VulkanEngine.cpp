12f19237508ceee547f4604c48122a2a3623a914
Alex Martin <sharkfinpro@hotmail.com>
2024-07-19 14:41:37 -0700

    Update VulkanEngine.cpp

--- source/VulkanEngine.cpp
+++ source/VulkanEngine.cpp
@@ -436,7 +436,6 @@ VkSurfaceFormatKHR VulkanEngine::chooseSwapSurfaceFormat(const std::vector<VkSur
 {
   for (const auto& availableFormat : availableFormats)
   {
-//    if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
     if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
     {
       return availableFormat;
