991ddd9a49e96ae2394b673bd3a18e98715e067e
Alex Martin <sharkfinpro@hotmail.com>
2024-07-20 18:56:57 -0700

    Create ShaderModule class

---
+++ source/pipeline/ShaderModule.h
@@ -0,0 +1,26 @@
+#ifndef VULKANPROJECT_SHADERMODULE_H
+#define VULKANPROJECT_SHADERMODULE_H
+
+#include <vulkan/vulkan.h>
+#include <vector>
+
+class ShaderModule {
+public:
+  ShaderModule(VkDevice& device, const char* filename, VkShaderStageFlagBits stage);
+  ~ShaderModule();
+
+  VkPipelineShaderStageCreateInfo getShaderStageCreateInfo();
+
+private:
+  std::vector<char> readFile(const char* filename);
+
+  void createShaderModule(const char* file);
+
+private:
+  VkDevice& device;
+  VkShaderModule module;
+  VkShaderStageFlagBits stage;
+};
+
+
+#endif //VULKANPROJECT_SHADERMODULE_H
