40315d0f67998c32d0ae1d4a32911ad186c40b80
Alex Martin <sharkfinpro@hotmail.com>
2024-08-16 12:19:33 -0700

    Update ComputePipeline.cpp

--- source/pipelines/ComputePipeline.cpp
+++ source/pipelines/ComputePipeline.cpp
@@ -179,7 +179,7 @@ void ComputePipeline::createShaderStorageBuffers(VkCommandPool& commandPool)
                   distribution(randomEngine), 1.0f);
   }
 
-  VkDeviceSize bufferSize = sizeof(Particle) * PARTICLE_COUNT;
+  const VkDeviceSize bufferSize = sizeof(Particle) * PARTICLE_COUNT;
 
   VkBuffer stagingBuffer;
   VkDeviceMemory stagingBufferMemory;
