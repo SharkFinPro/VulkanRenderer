00fff91569402df1be6a45f19504e67a0ec3eb58
Alex Martin <sharkfinpro@hotmail.com>
2024-08-16 20:13:18 -0700

    Update ComputePipeline.cpp

--- source/pipelines/ComputePipeline.cpp
+++ source/pipelines/ComputePipeline.cpp
@@ -284,19 +284,17 @@ void ComputePipeline::createGraphicsPipeline(VkRenderPass& renderPass)
 
 void ComputePipeline::createUniformBuffers()
 {
-  VkDeviceSize bufferSize = sizeof(UniformBufferObject);
-
   uniformBuffers.resize(MAX_FRAMES_IN_FLIGHT);
   uniformBuffersMemory.resize(MAX_FRAMES_IN_FLIGHT);
   uniformBuffersMapped.resize(MAX_FRAMES_IN_FLIGHT);
 
   for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++)
   {
+    constexpr VkDeviceSize bufferSize = sizeof(UniformBufferObject);
+
     Buffers::createBuffer(logicalDevice->getDevice(),
-                          physicalDevice->getPhysicalDevice(), bufferSize,
-                          VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
-                          VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
-                              VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                          physicalDevice->getPhysicalDevice(), bufferSize, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+                          VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
                           uniformBuffers[i], uniformBuffersMemory[i]);
 
     vkMapMemory(logicalDevice->getDevice(), uniformBuffersMemory[i], 0, bufferSize, 0, &uniformBuffersMapped[i]);
