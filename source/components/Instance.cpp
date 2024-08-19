4e317a6c45f5f2a870a13f255c39e3de3429f4bb
Alex Martin <sharkfinpro@hotmail.com>
2024-08-19 15:00:34 -0700

    Update Instance.cpp

--- source/components/Instance.cpp
+++ source/components/Instance.cpp
@@ -18,38 +18,32 @@ Instance::Instance()
     throw std::runtime_error("validation layers requested, but not available!");
   }
 
-  VkApplicationInfo appInfo{};
-  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-  appInfo.pApplicationName = "Vulkan Engine";
-  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-  appInfo.pEngineName = "No Engine";
-  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-  appInfo.apiVersion = VK_API_VERSION_1_0;
-
-  VkInstanceCreateInfo createInfo{};
-  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-  createInfo.pApplicationInfo = &appInfo;
+  constexpr VkApplicationInfo appInfo {
+    .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+    .pApplicationName = "Vulkan Engine",
+    .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+    .pEngineName = "No Engine",
+    .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+    .apiVersion = VK_API_VERSION_1_0
+  };
 
   const auto extensions = getRequiredExtensions();
-  createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
-  createInfo.ppEnabledExtensionNames = extensions.data();
 
+  VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
   if (enableValidationLayers)
   {
-    createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
-    createInfo.ppEnabledLayerNames = validationLayers.data();
-
-    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
     DebugMessenger::populateCreateInfo(debugCreateInfo);
-
-    createInfo.pNext = &debugCreateInfo;
   }
-  else
-  {
-    createInfo.enabledLayerCount = 0;
 
-    createInfo.pNext = nullptr;
-  }
+  const VkInstanceCreateInfo createInfo {
+    .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+    .pNext = enableValidationLayers ? &debugCreateInfo : nullptr,
+    .pApplicationInfo = &appInfo,
+    .enabledLayerCount = enableValidationLayers ? static_cast<uint32_t>(validationLayers.size()) : 0,
+    .ppEnabledLayerNames = enableValidationLayers ? validationLayers.data() : nullptr,
+    .enabledExtensionCount = static_cast<uint32_t>(extensions.size()),
+    .ppEnabledExtensionNames = extensions.data()
+  };
 
   if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
   {
