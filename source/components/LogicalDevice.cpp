6b4190a8342376e87f71195ce0c61ef42924217c
Alex Martin <sharkfinpro@hotmail.com>
2024-08-19 15:05:40 -0700

    Update LogicalDevice.cpp

--- source/components/LogicalDevice.cpp
+++ source/components/LogicalDevice.cpp
@@ -115,12 +115,14 @@ void LogicalDevice::createSyncObjects()
   inFlightFences.resize(MAX_FRAMES_IN_FLIGHT);
   computeInFlightFences.resize(MAX_FRAMES_IN_FLIGHT);
 
-  VkSemaphoreCreateInfo semaphoreInfo{};
-  semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+  VkSemaphoreCreateInfo semaphoreInfo {
+    .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO
+  };
 
-  VkFenceCreateInfo fenceInfo{};
-  fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
-  fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+  VkFenceCreateInfo fenceInfo {
+    .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+    .flags = VK_FENCE_CREATE_SIGNALED_BIT
+  };
 
   for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++)
   {
