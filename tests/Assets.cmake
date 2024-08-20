b6fa5ff829409bcf58ec3f10e948d0bcfaf0f412
Alex Martin <sharkfinpro@hotmail.com>
2024-08-19 20:44:52 -0700

    Revert "copy assets better"
    This reverts commit 9354f09093991b63af9768d667f839b777a5f6f0.

--- tests/Assets.cmake
+++
@@ -1,8 +0,0 @@
-add_custom_target(Assets ALL
-  COMMAND ${CMAKE_COMMAND} -E copy_directory
-  "${CMAKE_CURRENT_SOURCE_DIR}/../assets"
-  "${CMAKE_BINARY_DIR}/bin/assets"
-  COMMENT "Copying Assets"
-)
-
-add_dependencies(${PROJECT_NAME} Assets)
