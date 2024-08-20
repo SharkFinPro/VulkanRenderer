3f28026973629623871aaa96ed7ee64644830890
Alex Martin <sharkfinpro@hotmail.com>
2024-08-20 13:40:41 -0700

    fix msvc builds

--- source/pipelines/ComputePipeline.cpp
+++ source/pipelines/ComputePipeline.cpp
@@ -1,6 +1,7 @@
 #include "ComputePipeline.h"
 
 #include <utility>
+#include <stdexcept>
 
 ComputePipeline::ComputePipeline(const std::shared_ptr<PhysicalDevice> &physicalDevice,
                                  const std::shared_ptr<LogicalDevice> &logicalDevice)
