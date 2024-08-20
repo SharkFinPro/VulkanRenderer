d4600a7df119d3a0ef395f3a1daeefb5f2300b4b
Alex Martin <sharkfinpro@hotmail.com>
2024-08-20 12:02:39 -0700

    Update ComputePipeline.cpp

--- source/pipelines/ComputePipeline.cpp
+++ source/pipelines/ComputePipeline.cpp
@@ -26,39 +26,6 @@ void ComputePipeline::createShader(const char* filename)
 
 void ComputePipeline::loadDescriptorSetLayout(VkDescriptorSetLayout descriptorSetLayout)
 {
-  ///
-  // std::array<VkDescriptorSetLayoutBinding, 3> layoutBindings{};
-  // layoutBindings[0].binding = 0;
-  // layoutBindings[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-  // layoutBindings[0].descriptorCount = 1;
-  // layoutBindings[0].pImmutableSamplers = nullptr;
-  // layoutBindings[0].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
-  //
-  // layoutBindings[1].binding = 1;
-  // layoutBindings[1].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
-  // layoutBindings[1].descriptorCount = 1;
-  // layoutBindings[1].pImmutableSamplers = nullptr;
-  // layoutBindings[1].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
-  //
-  // layoutBindings[2].binding = 2;
-  // layoutBindings[2].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
-  // layoutBindings[2].descriptorCount = 1;
-  // layoutBindings[2].pImmutableSamplers = nullptr;
-  // layoutBindings[2].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
-  //
-  // VkDescriptorSetLayoutCreateInfo descriptorSetLayoutInfo{};
-  // descriptorSetLayoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-  // descriptorSetLayoutInfo.bindingCount = static_cast<uint32_t>(layoutBindings.size());
-  // descriptorSetLayoutInfo.pBindings = layoutBindings.data();
-  //
-  // if (vkCreateDescriptorSetLayout(logicalDevice->getDevice(), &descriptorSetLayoutInfo, nullptr,
-  //                                 &computeDescriptorSetLayout) != VK_SUCCESS)
-  // {
-  //   throw std::runtime_error("failed to create descriptor set layout!");
-  // }
-  ///
-
-
   descriptorSetLayouts.emplace_back(descriptorSetLayout);
 }
 
