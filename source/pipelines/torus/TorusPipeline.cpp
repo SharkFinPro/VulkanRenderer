3304ad3586b6a44cdacac75123e91531bb49ec60
Alex Martin <sharkfinpro@hotmail.com>
2024-08-22 11:11:17 -0700

    pipeline setup

--- source/pipelines/torus/TorusPipeline.cpp
+++ source/pipelines/torus/TorusPipeline.cpp
@@ -1,1 +1,8 @@
 #include "TorusPipeline.h"
+
+TorusPipeline::TorusPipeline(const std::shared_ptr<PhysicalDevice>& physicalDevice,
+                             const std::shared_ptr<LogicalDevice>& logicalDevice, const VkCommandPool& commandPool,
+                             const VkRenderPass& renderPass, const VkExtent2D& swapChainExtent)
+    : ComputePipeline(physicalDevice, logicalDevice), GraphicsPipeline(physicalDevice, logicalDevice)
+{
+}
--- source/pipelines/torus/TorusPipeline.h
+++ source/pipelines/torus/TorusPipeline.h
@@ -1,10 +1,28 @@
 #ifndef TORUSPIPELINE_H
 #define TORUSPIPELINE_H
 
+#include <cstdint>
 
+#include "../ComputePipeline.h"
+#include "../GraphicsPipeline.h"
 
-class TorusPipeline {
+struct MeshUniformBuffer {
+   float time;
+   uint32_t numU;
+   uint32_t numV;
+   float minU, maxU;
+   float minV, maxV;
+   uint32_t p1, p2;
+   uint32_t q1, q2;
+   float morph;
+};
 
+class TorusPipeline : public ComputePipeline, public GraphicsPipeline {
+public:
+  TorusPipeline(const std::shared_ptr<PhysicalDevice>& physicalDevice, const std::shared_ptr<LogicalDevice>& logicalDevice,
+                 const VkCommandPool& commandPool, const VkRenderPass& renderPass, const VkExtent2D& swapChainExtent);
+
+private:
 };
 
 
