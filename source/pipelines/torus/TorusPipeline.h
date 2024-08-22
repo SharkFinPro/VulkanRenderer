753098900c73437be97f5b10ad4d2f67b12b7deb
Alex Martin <sharkfinpro@hotmail.com>
2024-08-22 11:38:58 -0700

    Update TorusPipeline.h

--- source/pipelines/torus/TorusPipeline.h
+++ source/pipelines/torus/TorusPipeline.h
@@ -2,6 +2,7 @@
 #define TORUSPIPELINE_H
 
 #include <cstdint>
+#include <deque>
 
 #include "../ComputePipeline.h"
 #include "../GraphicsPipeline.h"
@@ -17,12 +18,40 @@ struct MeshUniformBuffer {
    float morph;
 };
 
+constexpr uint32_t numU = 1024;
+constexpr uint32_t numV = 1024;
+
+inline void generateIndices(uint32_t* indices)
+{
+  for (uint32_t j = 0; j < numV - 1; j++)
+  {
+    for (uint32_t i = 0; i < numU - 1; i++)
+    {
+      const uint32_t offset = (j * (numU - 1) + i) * 6;
+      const uint32_t i1 = (j + 0) * numU + (i + 0);
+      const uint32_t i2 = (j + 0) * numU + (i + 1);
+      const uint32_t i3 = (j + 1) * numU + (i + 1);
+      const uint32_t i4 = (j + 1) * numU + (i + 0);
+
+      indices[offset + 0] = i1;
+      indices[offset + 1] = i2;
+      indices[offset + 2] = i4;
+      indices[offset + 3] = i2;
+      indices[offset + 4] = i3;
+      indices[offset + 5] = i4;
+    }
+  }
+}
+
 class TorusPipeline : public ComputePipeline, public GraphicsPipeline {
 public:
   TorusPipeline(const std::shared_ptr<PhysicalDevice>& physicalDevice, const std::shared_ptr<LogicalDevice>& logicalDevice,
                 const VkCommandPool& commandPool, const VkRenderPass& renderPass, const VkExtent2D& swapChainExtent);
 
 private:
+  std::deque<std::pair<uint32_t, uint32_t>> morphQueue = { {5, 8 }, {5, 8} }
+  float morphCoef = 0.0f;
+  float animationSpeed = 1.0f;
 };
 
 
