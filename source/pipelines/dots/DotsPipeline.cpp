eb7464586ce183b98548de70a0385657112455cd
Alex Martin <sharkfinpro@hotmail.com>
2024-12-15 22:58:42 -0800

    disable depth test for dots pipeline

--- source/pipelines/dots/DotsPipeline.cpp
+++ source/pipelines/dots/DotsPipeline.cpp
@@ -126,13 +126,8 @@ std::unique_ptr<VkPipelineDepthStencilStateCreateInfo> DotsPipeline::defineDepth
 {
   VkPipelineDepthStencilStateCreateInfo depthStencilState {
     .sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO,
-    .depthTestEnable = VK_TRUE,
-    .depthWriteEnable = VK_TRUE,
-    .depthCompareOp = VK_COMPARE_OP_LESS,
-    .depthBoundsTestEnable = VK_FALSE,
-    .stencilTestEnable = VK_FALSE,
-    .minDepthBounds = 0.0f,
-    .maxDepthBounds = 1.0f
+    .depthTestEnable = VK_FALSE,
+    .depthWriteEnable = VK_FALSE
   };
 
   return std::make_unique<VkPipelineDepthStencilStateCreateInfo>(depthStencilState);
