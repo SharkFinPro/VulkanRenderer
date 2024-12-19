563f6d3e7eaac0018c4e131905be775752f53641
Alex Martin <sharkfinpro@hotmail.com>
2024-12-18 23:14:16 -0800

    Allow disabling of camera

--- source/VulkanEngine.cpp
+++ source/VulkanEngine.cpp
@@ -39,7 +39,7 @@ void VulkanEngine::render()
 {
   window->update();
 
-  if (sceneIsFocused())
+  if (sceneIsFocused() && useCamera)
   {
     camera->processInput(window);
     setCameraParameters(camera->getPosition(), camera->getViewMatrix());
