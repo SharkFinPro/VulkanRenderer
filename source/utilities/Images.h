cc726db02256088d04add9ab99f700023a6bacdb
Alex Martin <sharkfinpro@hotmail.com>
2025-02-05 23:23:42 -0800

    use namespace for images instead of class

--- source/utilities/Images.h
+++ source/utilities/Images.h
@@ -4,27 +4,26 @@
 #include <vulkan/vulkan.h>
 #include <memory>
 
-#include "../components/LogicalDevice.h"
+class LogicalDevice;
 
-class Images {
-public:
-  static void createImage(const VkDevice& device, const VkPhysicalDevice& physicalDevice, uint32_t width, uint32_t height,
-                          uint32_t depth, uint32_t mipLevels, VkSampleCountFlagBits numSamples, VkFormat format,
-                          VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image,
-                          VkDeviceMemory& imageMemory, VkImageType imageType);
+namespace Images
+{
+  void createImage(const VkDevice& device, const VkPhysicalDevice& physicalDevice, uint32_t width, uint32_t height,
+                   uint32_t depth, uint32_t mipLevels, VkSampleCountFlagBits numSamples, VkFormat format,
+                   VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image,
+                   VkDeviceMemory& imageMemory, VkImageType imageType);
 
-  static void transitionImageLayout(const std::shared_ptr<LogicalDevice>& logicalDevice, const VkCommandPool& commandPool,
-                                    VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout,
-                                    uint32_t mipLevels);
+  void transitionImageLayout(const std::shared_ptr<LogicalDevice>& logicalDevice, const VkCommandPool& commandPool,
+                             VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout,
+                             uint32_t mipLevels);
 
-  static void copyBufferToImage(const std::shared_ptr<LogicalDevice>& logicalDevice, const VkCommandPool& commandPool,
-                                VkBuffer buffer, VkImage image, uint32_t width, uint32_t height, uint32_t depth);
+  void copyBufferToImage(const std::shared_ptr<LogicalDevice>& logicalDevice, const VkCommandPool& commandPool,
+                         VkBuffer buffer, VkImage image, uint32_t width, uint32_t height, uint32_t depth);
 
-  static VkImageView createImageView(const VkDevice& device, VkImage image, VkFormat format,
-                                     VkImageAspectFlags aspectFlags, uint32_t mipLevels, VkImageViewType viewType);
+  VkImageView createImageView(const VkDevice& device, VkImage image, VkFormat format, VkImageAspectFlags aspectFlags,
+                              uint32_t mipLevels, VkImageViewType viewType);
 
-private:
-  static bool hasStencilComponent(VkFormat format);
+  bool hasStencilComponent(VkFormat format);
 };
 
 
