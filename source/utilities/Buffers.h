c32236c4fbeec5ecefe868b89077eec3a5c9ae36
Alex Martin <sharkfinpro@hotmail.com>
2025-02-05 23:28:28 -0800

    use a namespace for buffers

--- source/utilities/Buffers.h
+++ source/utilities/Buffers.h
@@ -3,22 +3,21 @@
 
 #include <vulkan/vulkan.h>
 
-class Buffers {
-public:
-  static uint32_t findMemoryType(const VkPhysicalDevice& physicalDevice, uint32_t typeFilter,
-                                 VkMemoryPropertyFlags properties);
+namespace Buffers
+{
+  uint32_t findMemoryType(const VkPhysicalDevice& physicalDevice, uint32_t typeFilter, VkMemoryPropertyFlags properties);
 
-  static void createBuffer(const VkDevice& device, const VkPhysicalDevice& physicalDevice, VkDeviceSize size,
-                           VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer,
-                           VkDeviceMemory& bufferMemory);
+  void createBuffer(const VkDevice& device, const VkPhysicalDevice& physicalDevice, VkDeviceSize size,
+                    VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer,
+                    VkDeviceMemory& bufferMemory);
 
-  static void copyBuffer(const VkDevice& device, const VkCommandPool& commandPool, const VkQueue& queue, VkBuffer srcBuffer,
-                         VkBuffer dstBuffer, VkDeviceSize size);
+  void copyBuffer(const VkDevice& device, const VkCommandPool& commandPool, const VkQueue& queue, VkBuffer srcBuffer,
+                  VkBuffer dstBuffer, VkDeviceSize size);
 
-  static VkCommandBuffer beginSingleTimeCommands(const VkDevice& device, const VkCommandPool& commandPool);
+  VkCommandBuffer beginSingleTimeCommands(const VkDevice& device, const VkCommandPool& commandPool);
 
-  static void endSingleTimeCommands(const VkDevice& device, const VkCommandPool& commandPool, const VkQueue& queue,
-                                    VkCommandBuffer commandBuffer);
+  void endSingleTimeCommands(const VkDevice& device, const VkCommandPool& commandPool, const VkQueue& queue,
+                             VkCommandBuffer commandBuffer);
 };
 
 
