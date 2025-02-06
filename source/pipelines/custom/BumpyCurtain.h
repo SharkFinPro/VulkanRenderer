5b8e2874e30176d1287fcca5a826d9fe59a4be82
Alex Martin <sharkfinpro@hotmail.com>
2025-02-05 19:11:21 -0800

    Create bumpy curtain pipeline

---
+++ source/pipelines/custom/BumpyCurtain.h
@@ -0,0 +1,101 @@
+#ifndef BUMPYCURTAIN_H
+#define BUMPYCURTAIN_H
+
+#include <vulkan/vulkan.h>
+#include <glm/glm.hpp>
+#include <memory>
+#include <vector>
+#include <array>
+
+#include "Uniforms.h"
+#include "../GraphicsPipeline.h"
+
+class PhysicalDevice;
+class LogicalDevice;
+class RenderPass;
+class RenderObject;
+class Camera;
+class UniformBuffer;
+class Light;
+class Noise3DTexture;
+
+class BumpyCurtain final : public GraphicsPipeline {
+public:
+  BumpyCurtain(const std::shared_ptr<PhysicalDevice>& physicalDevice,
+                      const std::shared_ptr<LogicalDevice>& logicalDevice,
+                      const std::shared_ptr<RenderPass>& renderPass, const VkCommandPool& commandPool);
+  ~BumpyCurtain() override;
+
+  VkDescriptorSetLayout& getLayout();
+
+  void render(const VkCommandBuffer& commandBuffer, uint32_t currentFrame, glm::vec3 viewPosition,
+              const glm::mat4& viewMatrix, VkExtent2D swapChainExtent,
+              const std::vector<std::shared_ptr<Light>>& lights,
+              const std::vector<std::shared_ptr<RenderObject>>& objects);
+
+private:
+  CurtainUniform curtainUBO {
+    .amplitude = 0.1,
+    .period = 1,
+    .shininess = 10
+  };
+
+  NoiseOptionsUniform noiseOptionsUBO {
+    .amplitude = 0.5f,
+    .frequency = 1.0f
+  };
+
+  VkDescriptorSetLayout globalDescriptorSetLayout;
+  VkDescriptorSetLayout objectDescriptorSetLayout;
+
+  VkDescriptorPool descriptorPool;
+  std::vector<VkDescriptorSet> descriptorSets;
+
+  std::unique_ptr<UniformBuffer> lightMetadataUniform;
+  std::unique_ptr<UniformBuffer> lightsUniform;
+  std::unique_ptr<UniformBuffer> cameraUniform;
+  std::unique_ptr<UniformBuffer> curtainUniform;
+  std::unique_ptr<UniformBuffer> noiseOptionsUniform;
+  std::unique_ptr<Noise3DTexture> noiseTexture;
+
+  int prevNumLights = 0;
+
+  size_t lightsUniformBufferSize;
+
+  VkPipelineColorBlendAttachmentState colorBlendAttachment;
+
+  std::array<VkDynamicState, 2> dynamicStates;
+
+  VkVertexInputBindingDescription vertexBindingDescription;
+  std::array<VkVertexInputAttributeDescription, 3> vertexAttributeDescriptions;
+
+  void loadGraphicsShaders() override;
+
+  void loadGraphicsDescriptorSetLayouts() override;
+
+  std::unique_ptr<VkPipelineColorBlendStateCreateInfo> defineColorBlendState() override;
+  std::unique_ptr<VkPipelineDepthStencilStateCreateInfo> defineDepthStencilState() override;
+  std::unique_ptr<VkPipelineDynamicStateCreateInfo> defineDynamicState() override;
+  std::unique_ptr<VkPipelineInputAssemblyStateCreateInfo> defineInputAssemblyState() override;
+  std::unique_ptr<VkPipelineMultisampleStateCreateInfo> defineMultisampleState() override;
+  std::unique_ptr<VkPipelineRasterizationStateCreateInfo> defineRasterizationState() override;
+  std::unique_ptr<VkPipelineVertexInputStateCreateInfo> defineVertexInputState() override;
+  std::unique_ptr<VkPipelineViewportStateCreateInfo> defineViewportState() override;
+
+  void createDescriptorSetLayouts();
+
+  void createGlobalDescriptorSetLayout();
+  void createObjectDescriptorSetLayout();
+
+  void createDescriptorPool();
+
+  void createDescriptorSets();
+
+  void createUniforms(const VkCommandPool& commandPool);
+
+  void updateLightUniforms(const std::vector<std::shared_ptr<Light>>& lights, uint32_t currentFrame);
+};
+
+
+
+#endif //BUMPYCURTAIN_H
