b6e54675431cdc013687b6bd10865c69ae134ecb
Alex Martin <sharkfinpro@hotmail.com>
2025-02-13 19:53:08 -0800

    don't cull plane

--- source/pipelines/custom/TexturedPlane.cpp
+++ source/pipelines/custom/TexturedPlane.cpp
@@ -185,7 +185,7 @@ std::unique_ptr<VkPipelineRasterizationStateCreateInfo> TexturedPlane::defineRas
     .depthClampEnable = VK_FALSE,
     .rasterizerDiscardEnable = VK_FALSE,
     .polygonMode = VK_POLYGON_MODE_FILL,
-    .cullMode = VK_CULL_MODE_BACK_BIT,
+    .cullMode = VK_CULL_MODE_NONE,
     .frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE,
     .depthBiasEnable = VK_FALSE,
     .lineWidth = 1.0f
