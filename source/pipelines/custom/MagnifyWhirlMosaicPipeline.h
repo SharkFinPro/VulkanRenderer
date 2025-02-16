16b269bbd43d68c2ef3714d7c6f7f9a9df835a44
Alex Martin <sharkfinpro@hotmail.com>
2025-02-15 21:14:48 -0800

    populate pipeline

--- source/pipelines/custom/MagnifyWhirlMosaicPipeline.h
+++ source/pipelines/custom/MagnifyWhirlMosaicPipeline.h
@@ -1,10 +1,62 @@
 #ifndef MAGNIFYWHIRLMOSAIC_H
 #define MAGNIFYWHIRLMOSAIC_H
 
+#include "../GraphicsPipeline.h"
+#include <vulkan/vulkan.h>>
+#include <glm/glm.hpp>
+#include <vector>
+#include <memory>
 
+class LogicalDevice;
+class PhysicalDevice;
+class RenderPass;
+class RenderObject;
+class UniformBuffer;
+class Light;
 
-class MagnifyWhirlMosaicPipeline {
+class MagnifyWhirlMosaicPipeline final : public GraphicsPipeline {
+public:
+  MagnifyWhirlMosaicPipeline(const std::shared_ptr<PhysicalDevice>& physicalDevice,
+                             const std::shared_ptr<LogicalDevice>& logicalDevice,
+                             const std::shared_ptr<RenderPass>& renderPass,
+                             VkDescriptorPool descriptorPool,
+                             VkDescriptorSetLayout objectDescriptorSetLayout);
 
+  ~MagnifyWhirlMosaicPipeline() override;
+
+  void render(const VkCommandBuffer& commandBuffer, uint32_t currentFrame, glm::vec3 viewPosition,
+              const glm::mat4& viewMatrix, VkExtent2D swapChainExtent,
+              const std::vector<std::shared_ptr<Light>>& lights,
+              const std::vector<std::shared_ptr<RenderObject>>& objects);
+
+private:
+  VkDescriptorPool descriptorPool = VK_NULL_HANDLE;
+  std::vector<VkDescriptorSet> descriptorSets;
+
+  VkDescriptorSetLayout globalDescriptorSetLayout = VK_NULL_HANDLE;
+  VkDescriptorSetLayout objectDescriptorSetLayout = VK_NULL_HANDLE;
+
+  std::unique_ptr<UniformBuffer> lightMetadataUniform;
+  std::unique_ptr<UniformBuffer> lightsUniform;
+  std::unique_ptr<UniformBuffer> cameraUniform;
+
+  int prevNumLights = 0;
+
+  size_t lightsUniformBufferSize = 0;
+
+  void loadGraphicsShaders() override;
+
+  void loadGraphicsDescriptorSetLayouts() override;
+
+  void defineStates() override;
+
+  void createGlobalDescriptorSetLayout();
+
+  void createDescriptorSets();
+
+  void createUniforms();
+
+  void updateLightUniforms(const std::vector<std::shared_ptr<Light>>& lights, uint32_t currentFrame);
 };
 
 
