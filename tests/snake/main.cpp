ab8a0a3b4cc636d791cd97fd4c8fcd995fc42918
Alex Martin <sharkfinpro@hotmail.com>
2025-02-20 15:13:27 -0800

    Update main.cpp

--- tests/snake/main.cpp
+++ tests/snake/main.cpp
@@ -16,7 +16,7 @@ int main()
       .WINDOW_WIDTH = 800,
       .WINDOW_HEIGHT = 600,
       .WINDOW_TITLE = "Snake",
-      .CAMERA_POSITION = { 0.0f, 0.0f, -5.0f },
+      .CAMERA_POSITION = { 0.0f, 0.0f, -15.0f },
       .DO_DOTS = false
     };
 
@@ -30,11 +30,11 @@ int main()
     const auto model = renderer.loadModel("assets/models/snake.obj");
 
     const auto object = renderer.loadRenderObject(texture, specularMap, model);
-    object->setPosition({ 0, -5, 0 });
+    object->setPosition({ 0, 0, 0 });
 
     std::vector<std::shared_ptr<Light>> lights;
 
-    lights.push_back(renderer.createLight({0, -3.5f, 0}, {1.0f, 1.0f, 1.0f}, 0.1f, 0.5f, 1.0f));
+    lights.push_back(renderer.createLight({0, 15.0f, 0}, {1.0f, 1.0f, 1.0f}, 0.1f, 0.5f, 1.0f));
 
     lights.push_back(renderer.createLight({5.0f, -3.5f, 5.0f}, {1.0f, 1.0f, 0}, 0, 0.5f, 1.0f));
 
