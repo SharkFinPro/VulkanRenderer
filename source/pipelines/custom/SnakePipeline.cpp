9727549b92b60a2ebd0085c839ad95eac47386a7
Alex Martin <sharkfinpro@hotmail.com>
2025-02-20 15:13:46 -0800

    create SnakePipeline class

---
+++ source/pipelines/custom/SnakePipeline.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Shark on 2/20/2025.
+//
+
+#include "SnakePipeline.h"
---
+++ source/pipelines/custom/SnakePipeline.h
@@ -0,0 +1,16 @@
+//
+// Created by Shark on 2/20/2025.
+//
+
+#ifndef SNAKEPIPELINE_H
+#define SNAKEPIPELINE_H
+
+
+
+class SnakePipeline {
+
+};
+
+
+
+#endif //SNAKEPIPELINE_H
