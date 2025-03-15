337d4d5210e6aebaa28cea1fb797a7834cecd452
Alex Martin <sharkfinpro@hotmail.com>
2025-03-15 11:30:30 -0700

    create smokeParticle

---
+++ source/pipelines/SmokeParticle.h
@@ -0,0 +1,41 @@
+#ifndef SMOKEPARTICLE_H
+#define SMOKEPARTICLE_H
+
+#include <vulkan/vulkan.h>
+#include <array>
+#include <glm/glm.hpp>
+
+struct Particle {
+  glm::vec3 position;
+  glm::vec3 velocity;
+  glm::vec4 color;
+
+  static constexpr VkVertexInputBindingDescription getBindingDescription()
+  {
+    return {
+      .binding = 0,
+      .stride = sizeof(Particle),
+      .inputRate = VK_VERTEX_INPUT_RATE_VERTEX
+    };
+  }
+
+  static constexpr std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions()
+  {
+    return {{
+      {
+        .location = 0,
+        .binding = 0,
+        .format = VK_FORMAT_R32G32_SFLOAT,
+        .offset = offsetof(Particle, position)
+      },
+      {
+        .location = 1,
+        .binding = 0,
+        .format = VK_FORMAT_R32G32B32A32_SFLOAT,
+        .offset = offsetof(Particle, color)
+      }
+    }};
+  }
+};
+
+#endif //SMOKEPARTICLE_H
