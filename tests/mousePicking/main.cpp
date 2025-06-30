3618da7dacb7b1fab5568567bd03e6296b182e5f
Alex Martin <sharkfinpro@hotmail.com>
2025-06-30 00:03:38 -0700

    separate responsibility

--- tests/mousePicking/main.cpp
+++ tests/mousePicking/main.cpp
@@ -4,14 +4,19 @@
 #include <imgui.h>
 #include <string>
 
-void displayObjectGui(const std::shared_ptr<RenderObject>& object);
-void displayLightGui(const std::shared_ptr<Light>& light, int id);
-
 struct MousePickingObject {
   std::shared_ptr<RenderObject> object;
   bool hovering = false;
   bool selected = false;
 };
+
+void displayObjectGui(const std::shared_ptr<RenderObject>& object);
+void displayLightGui(const std::shared_ptr<Light>& light, int id);
+void setupScene(VulkanEngine& renderer, std::vector<MousePickingObject>& objects,
+                std::vector<std::shared_ptr<Light>>& lights);
+
+void renderScene(VulkanEngine& renderer, const std::shared_ptr<ImGuiInstance>& gui, std::vector<MousePickingObject>& objects,
+                const std::vector<std::shared_ptr<Light>>& lights);
 
 int main()
 {
@@ -30,35 +35,9 @@ int main()
 
     ImGui::SetCurrentContext(VulkanEngine::getImGuiContext());
 
-    const auto texture = renderer.loadTexture("assets/textures/white.png");
-    const auto specularMap = renderer.loadTexture("assets/textures/blank_specular.png");
-    const auto model = renderer.loadModel("assets/models/square.glb");
-
     std::vector<MousePickingObject> objects;
-
-    const auto object1 = renderer.loadRenderObject(texture, specularMap, model);
-    object1->setPosition({ 0, -5, 0 });
-    objects.push_back({ object1 });
-
-    const auto object2 = renderer.loadRenderObject(texture, specularMap, model);
-    object2->setPosition({ -5, -10, 0 });
-    objects.push_back({ object2 });
-
-    const auto object3 = renderer.loadRenderObject(texture, specularMap, model);
-    object3->setPosition({ 10, 0, 15 });
-    objects.push_back({ object3 });
-
     std::vector<std::shared_ptr<Light>> lights;
-
-    lights.push_back(renderer.createLight({0, -3.5f, 0}, {1.0f, 1.0f, 1.0f}, 0.1f, 0.5f, 1.0f));
-
-    lights.push_back(renderer.createLight({5.0f, -3.5f, 5.0f}, {1.0f, 1.0f, 0}, 0, 0.5f, 1.0f));
-
-    lights.push_back(renderer.createLight({-5.0f, -3.5f, -5.0f}, {0.5f, 0.5f, 1.0f}, 0, 0.5f, 1.0f));
-
-    lights.push_back(renderer.createLight({5.0f, -3.5f, -5.0f}, {0, 1.0f, 0}, 0, 0.5f, 1.0f));
-
-    lights.push_back(renderer.createLight({-5.0f, -3.5f, 5.0f}, {1.0f, 0.5f, 1.0f}, 0, 0.5f, 1.0f));
+    setupScene(renderer, objects, lights);
 
     while (renderer.isActive())
     {
@@ -70,50 +49,7 @@ int main()
         }
       }
 
-      gui->dockCenter("SceneView");
-      gui->dockBottom("Selected Object");
-      gui->dockBottom("Lights");
-      gui->dockBottom("Elliptical Dots");
-      gui->dockBottom("Noisy Elliptical Dots");
-
-      gui->setBottomDockPercent(0.3);
-
-      // Render GUI
-      ImGui::Begin("Selected Object");
-      for (auto& [object, _, selected] : objects)
-      {
-        if (selected)
-        {
-          displayObjectGui(object);
-        }
-      }
-      ImGui::End();
-
-      ImGui::Begin("Lights");
-      for (int i = 0; i < lights.size(); i++)
-      {
-        displayLightGui(lights[i], i);
-      }
-      ImGui::End();
-
-      // Render Objects
-      for (auto& [object, hovering, selected] : objects)
-      {
-        renderer.renderObject(object, PipelineType::object, &hovering);
-
-        if (selected)
-        {
-          renderer.renderObject(object, PipelineType::objectHighlight);
-        }
-      }
-
-      for (const auto& light : lights)
-      {
-        renderer.renderLight(light);
-      }
-
-      // Render Frame
-      renderer.render();
+      renderScene(renderer, gui, objects, lights);
     }
   }
   catch (const std::exception& e)
@@ -167,4 +103,84 @@ void displayLightGui(const std::shared_ptr<Light>& light, const int id)
   light->setAmbient(ambient);
   light->setDiffuse(diffuse);
   light->setSpecular(specular);
+}
+
+void setupScene(VulkanEngine& renderer, std::vector<MousePickingObject>& objects,
+                std::vector<std::shared_ptr<Light>>& lights)
+{
+  const auto texture = renderer.loadTexture("assets/textures/white.png");
+  const auto specularMap = renderer.loadTexture("assets/textures/blank_specular.png");
+  const auto model = renderer.loadModel("assets/models/square.glb");
+
+  const auto object1 = renderer.loadRenderObject(texture, specularMap, model);
+  object1->setPosition({ 0, -5, 0 });
+  objects.push_back({ object1 });
+
+  const auto object2 = renderer.loadRenderObject(texture, specularMap, model);
+  object2->setPosition({ -5, -10, 0 });
+  objects.push_back({ object2 });
+
+  const auto object3 = renderer.loadRenderObject(texture, specularMap, model);
+  object3->setPosition({ 10, 0, 15 });
+  objects.push_back({ object3 });
+
+
+  lights.push_back(renderer.createLight({0, -3.5f, 0}, {1.0f, 1.0f, 1.0f}, 0.1f, 0.5f, 1.0f));
+
+  lights.push_back(renderer.createLight({5.0f, -3.5f, 5.0f}, {1.0f, 1.0f, 0}, 0, 0.5f, 1.0f));
+
+  lights.push_back(renderer.createLight({-5.0f, -3.5f, -5.0f}, {0.5f, 0.5f, 1.0f}, 0, 0.5f, 1.0f));
+
+  lights.push_back(renderer.createLight({5.0f, -3.5f, -5.0f}, {0, 1.0f, 0}, 0, 0.5f, 1.0f));
+
+  lights.push_back(renderer.createLight({-5.0f, -3.5f, 5.0f}, {1.0f, 0.5f, 1.0f}, 0, 0.5f, 1.0f));
+}
+
+void renderScene(VulkanEngine& renderer, const std::shared_ptr<ImGuiInstance>& gui, std::vector<MousePickingObject>& objects,
+                const std::vector<std::shared_ptr<Light>>& lights)
+{
+  gui->dockCenter("SceneView");
+  gui->dockBottom("Selected Object");
+  gui->dockBottom("Lights");
+  gui->dockBottom("Elliptical Dots");
+  gui->dockBottom("Noisy Elliptical Dots");
+
+  gui->setBottomDockPercent(0.3);
+
+  // Render GUI
+  ImGui::Begin("Selected Object");
+  for (auto& [object, _, selected] : objects)
+  {
+    if (selected)
+    {
+      displayObjectGui(object);
+    }
+  }
+  ImGui::End();
+
+  ImGui::Begin("Lights");
+  for (int i = 0; i < lights.size(); i++)
+  {
+    displayLightGui(lights[i], i);
+  }
+  ImGui::End();
+
+  // Render Objects
+  for (auto& [object, hovering, selected] : objects)
+  {
+    renderer.renderObject(object, PipelineType::object, &hovering);
+
+    if (selected)
+    {
+      renderer.renderObject(object, PipelineType::objectHighlight);
+    }
+  }
+
+  for (const auto& light : lights)
+  {
+    renderer.renderLight(light);
+  }
+
+  // Render Frame
+  renderer.render();
 }
