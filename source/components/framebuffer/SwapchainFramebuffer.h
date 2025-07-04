553b61fb14a49c8dff21cba2d40e19e8645ddee2
Alex Martin <sharkfinpro@hotmail.com>
2025-07-03 17:34:44 -0700

    create new class

---
+++ source/components/framebuffer/SwapchainFramebuffer.h
@@ -0,0 +1,12 @@
+#ifndef SWAPCHAINFRAMEBUFFER_H
+#define SWAPCHAINFRAMEBUFFER_H
+
+#include "Framebuffer.h"
+
+class SwapchainFramebuffer final : public Framebuffer {
+
+};
+
+
+
+#endif //SWAPCHAINFRAMEBUFFER_H
