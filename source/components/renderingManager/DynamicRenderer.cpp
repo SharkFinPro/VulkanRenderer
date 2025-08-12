7a27bbc71cc1b076d804d554ee4862146ea31901
Alex Martin <sharkfinpro@hotmail.com>
2025-08-11 23:56:31 -0700

    Update DynamicRenderer.cpp

--- source/components/renderingManager/DynamicRenderer.cpp
+++ source/components/renderingManager/DynamicRenderer.cpp
@@ -23,17 +23,57 @@ void DynamicRenderer::resetOffscreenImageResources(const VkExtent2D offscreenVie
 }
 
 void DynamicRenderer::beginSwapchainRendering(const uint32_t imageIndex, const VkExtent2D extent,
-                                              std::shared_ptr<CommandBuffer> commandBuffer)
+                                              const std::shared_ptr<CommandBuffer> commandBuffer)
 {
-  VkRenderingInfo renderingInfo {};
+  const VkRenderingInfo renderingInfo {
+    .sType = VK_STRUCTURE_TYPE_RENDERING_INFO,
+    .renderArea = {
+      .offset = {0, 0},
+      .extent = extent,
+    }
+  };
 
   commandBuffer->beginRendering(renderingInfo);
 }
 
 void DynamicRenderer::beginOffscreenRendering(const uint32_t imageIndex, const VkExtent2D extent,
-                                              std::shared_ptr<CommandBuffer> commandBuffer)
+                                              const std::shared_ptr<CommandBuffer> commandBuffer)
 {
-  VkRenderingInfo renderingInfo {};
+  VkRenderingAttachmentInfo colorRenderingAttachmentInfo {
+    .sType = VK_STRUCTURE_TYPE_RENDERING_ATTACHMENT_INFO,
+    .imageView = VK_NULL_HANDLE,
+    .imageLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+    .loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+    .storeOp = VK_ATTACHMENT_STORE_OP_STORE,
+    .clearValue {
+      .color = {0.0f, 0.0f, 0.0f, 1.0f},
+      .depthStencil = {1.0f, 0}
+    }
+  };
+
+  VkRenderingAttachmentInfo depthRenderingAttachmentInfo {
+    .sType = VK_STRUCTURE_TYPE_RENDERING_ATTACHMENT_INFO,
+    .imageView = VK_NULL_HANDLE,
+    .imageLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+    .loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+    .storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+    .clearValue {
+      .color = {0.0f, 0.0f, 0.0f, 1.0f},
+      .depthStencil = {1.0f, 0}
+    }
+  };
+
+  const VkRenderingInfo renderingInfo {
+    .sType = VK_STRUCTURE_TYPE_RENDERING_INFO,
+    .renderArea = {
+      .offset = {0, 0},
+      .extent = extent,
+    },
+    .layerCount = 1,
+    .colorAttachmentCount = 1,
+    .pColorAttachments = &colorRenderingAttachmentInfo,
+    .pDepthAttachment = &depthRenderingAttachmentInfo,
+  };
 
   commandBuffer->beginRendering(renderingInfo);
 }
