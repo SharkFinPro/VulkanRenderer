01f548dd2f981a5b72f920b6529605a87bf46a9b
Alex Martin <sharkfinpro@hotmail.com>
2025-08-11 23:15:06 -0700

    Implement DynamicRenderer class skeleton
    Added the initial implementation of the DynamicRenderer class, including method stubs for rendering operations and resource management. This sets up the structure for dynamic rendering, with integration points for command buffers and swapchain/offscreen resources.

--- source/components/renderingManager/DynamicRenderer.cpp
+++ source/components/renderingManager/DynamicRenderer.cpp
@@ -1,1 +1,44 @@
 #include "DynamicRenderer.h"
+#include "../core/commandBuffer/CommandBuffer.h"
+
+DynamicRenderer::DynamicRenderer(const std::shared_ptr<LogicalDevice>& logicalDevice)
+  : Renderer(logicalDevice)
+{}
+
+std::shared_ptr<RenderPass> DynamicRenderer::getRenderPass() const
+{
+  return nullptr;
+}
+
+VkDescriptorSet& DynamicRenderer::getOffscreenImageDescriptorSet(const uint32_t imageIndex) const
+{
+}
+
+void DynamicRenderer::resetSwapchainImageResources(std::shared_ptr<SwapChain> swapChain)
+{
+}
+
+void DynamicRenderer::resetOffscreenImageResources(const VkExtent2D offscreenViewportExtent)
+{
+}
+
+void DynamicRenderer::beginSwapchainRendering(const uint32_t imageIndex, const VkExtent2D extent,
+                                              std::shared_ptr<CommandBuffer> commandBuffer)
+{
+  VkRenderingInfo renderingInfo {};
+
+  commandBuffer->beginRendering(renderingInfo);
+}
+
+void DynamicRenderer::beginOffscreenRendering(const uint32_t imageIndex, const VkExtent2D extent,
+                                              std::shared_ptr<CommandBuffer> commandBuffer)
+{
+  VkRenderingInfo renderingInfo {};
+
+  commandBuffer->beginRendering(renderingInfo);
+}
+
+void DynamicRenderer::endRendering(std::shared_ptr<CommandBuffer> commandBuffer)
+{
+  commandBuffer->endRendering();
+}
--- source/components/renderingManager/DynamicRenderer.h
+++ source/components/renderingManager/DynamicRenderer.h
@@ -4,7 +4,24 @@
 #include "Renderer.h"
 
 class DynamicRenderer final : public Renderer {
+public:
+  explicit DynamicRenderer(const std::shared_ptr<LogicalDevice>& logicalDevice);
 
+  [[nodiscard]] std::shared_ptr<RenderPass> getRenderPass() const override;
+
+  [[nodiscard]] VkDescriptorSet& getOffscreenImageDescriptorSet(uint32_t imageIndex) const override;
+
+  void resetSwapchainImageResources(std::shared_ptr<SwapChain> swapChain) override;
+
+  void resetOffscreenImageResources(VkExtent2D offscreenViewportExtent) override;
+
+  void beginSwapchainRendering(uint32_t imageIndex, VkExtent2D extent,
+                               std::shared_ptr<CommandBuffer> commandBuffer) override;
+
+  void beginOffscreenRendering(uint32_t imageIndex, VkExtent2D extent,
+                               std::shared_ptr<CommandBuffer> commandBuffer) override;
+
+  void endRendering(std::shared_ptr<CommandBuffer> commandBuffer) override;
 };
 
 
