0c7b612ef907681895caa8d52ed8f4c0468fefbc
Alex Martin <sharkfinpro@hotmail.com>
2025-12-16 22:49:00 -0800

    Implement PointLightShadowMapPipeline class
    Added the PointLightShadowMapPipeline class inheriting from GraphicsPipeline, including its constructor, render, and updateUniformVariables methods. This sets up the pipeline for point light shadow mapping and integrates it with the rendering framework.

--- source/components/pipelines/implementations/renderObject/PointLightShadowMapPipeline.cpp
+++ source/components/pipelines/implementations/renderObject/PointLightShadowMapPipeline.cpp
@@ -1,4 +1,57 @@
 #include "PointLightShadowMapPipeline.h"
+#include "../common/GraphicsPipelineStates.h"
+#include "../../../assets/objects/RenderObject.h"
+#include "../../../commandBuffer/CommandBuffer.h"
 
 namespace vke {
+  PointLightShadowMapPipeline::PointLightShadowMapPipeline(const std::shared_ptr<LogicalDevice>& logicalDevice,
+                                                           std::shared_ptr<RenderPass> renderPass,
+                                                           VkDescriptorSetLayout objectDescriptorSetLayout)
+    : GraphicsPipeline(logicalDevice)
+  {
+    const GraphicsPipelineOptions graphicsPipelineOptions{
+      .shaders{
+        .vertexShader = "assets/shaders/ShadowCubeMap.vert.spv",
+      },
+      .states{
+        .colorBlendState = GraphicsPipelineStates::colorBlendStateShadow,
+        .depthStencilState = GraphicsPipelineStates::depthStencilState,
+        .dynamicState = GraphicsPipelineStates::dynamicState,
+        .inputAssemblyState = GraphicsPipelineStates::inputAssemblyStateTriangleList,
+        .multisampleState = GraphicsPipelineStates::multisampleStateNone,
+        .rasterizationState = GraphicsPipelineStates::rasterizationStateCullBack,
+        .vertexInputState = GraphicsPipelineStates::vertexInputStateVertex,
+        .viewportState = GraphicsPipelineStates::viewportState
+      },
+      .descriptorSetLayouts{
+        objectDescriptorSetLayout
+      },
+      .renderPass = renderPass
+    };
+
+    createPipeline(graphicsPipelineOptions, false);
+  }
+
+  void PointLightShadowMapPipeline::render(const RenderInfo* renderInfo,
+                                           const std::vector<std::shared_ptr<RenderObject>>* objects)
+  {
+    GraphicsPipeline::render(renderInfo, nullptr);
+
+    updateUniformVariables(renderInfo);
+
+    if (objects)
+    {
+      for (const auto& object : *objects)
+      {
+        object->updateUniformBuffer(renderInfo->currentFrame, {1.0}, {1.0});
+
+        object->draw(renderInfo->commandBuffer, m_pipelineLayout, renderInfo->currentFrame, 0);
+      }
+    }
+  }
+
+  void PointLightShadowMapPipeline::updateUniformVariables(const RenderInfo* renderInfo)
+  {
+    // TODO: Update cube map images uniform
+  }
 } // vke
--- source/components/pipelines/implementations/renderObject/PointLightShadowMapPipeline.h
+++ source/components/pipelines/implementations/renderObject/PointLightShadowMapPipeline.h
@@ -1,8 +1,18 @@
 #ifndef VULKANPROJECT_POINTLIGHTSHADOWMAPPIPELINE_H
 #define VULKANPROJECT_POINTLIGHTSHADOWMAPPIPELINE_H
 
+#include "../../GraphicsPipeline.h"
+
 namespace vke {
-  class PointLightShadowMapPipeline {
+  class PointLightShadowMapPipeline : public GraphicsPipeline {
+  public:
+    PointLightShadowMapPipeline(const std::shared_ptr<LogicalDevice>& logicalDevice,
+                                std::shared_ptr<RenderPass> renderPass,
+                                VkDescriptorSetLayout objectDescriptorSetLayout);
+
+    void render(const RenderInfo* renderInfo, const std::vector<std::shared_ptr<RenderObject>>* objects) override;
+
+    void updateUniformVariables(const RenderInfo* renderInfo) override;
   };
 } // vke
 
