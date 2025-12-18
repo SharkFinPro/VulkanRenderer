f4852651c1decea3a04498868b1a55ac115b923d
Alex Martin <sharkfinpro@hotmail.com>
2025-12-18 15:51:04 -0800

    Add fill color and transform stack to Renderer2D
    Introduced m_currentFill, m_currentTransform, and m_transformStack members to the Renderer2D class for managing fill color and transformation state. Also added necessary GLM and vector includes.

--- source/components/renderingManager/renderer2D/Renderer2D.h
+++ source/components/renderingManager/renderer2D/Renderer2D.h
@@ -1,8 +1,19 @@
 #ifndef VULKANPROJECT_RENDERER2D_H
 #define VULKANPROJECT_RENDERER2D_H
 
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+#include <vector>
+
 namespace vke {
   class Renderer2D {
+
+  private:
+    glm::vec4 m_currentFill = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+
+    glm::mat4 m_currentTransform = glm::mat4(1.0f);
+
+    std::vector<glm::mat4> m_transformStack;
   };
 } // vke
 
