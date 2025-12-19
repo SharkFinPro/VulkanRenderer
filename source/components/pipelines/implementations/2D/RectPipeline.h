1bc5ade778ef7a10696463ddb90ce44654eaf489
Alex Martin <sharkfinpro@hotmail.com>
2025-12-18 16:06:49 -0800

    Add Rect and RectPushConstant structs to RectPipeline
    Introduced Rect and RectPushConstant structures with relevant glm types to support rectangle rendering and push constant data in the RectPipeline.

--- source/components/pipelines/implementations/2D/RectPipeline.h
+++ source/components/pipelines/implementations/2D/RectPipeline.h
@@ -1,7 +1,25 @@
 #ifndef VULKANPROJECT_RECTPIPELINE_H
 #define VULKANPROJECT_RECTPIPELINE_H
 
+#include <glm/vec2.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 namespace vke {
+
+  struct Rect {
+    glm::vec4 bounds;
+    glm::vec4 color;
+    glm::mat4 transform;
+  };
+
+  struct RectPushConstant {
+    glm::mat4 transform;
+    glm::vec2 screenSize;
+    glm::vec4 bounds;
+    glm::vec4 color;
+  };
+
   class RectPipeline {
   };
 } // vke
