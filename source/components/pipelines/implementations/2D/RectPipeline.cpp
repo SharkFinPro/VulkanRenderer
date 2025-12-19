1b998153204b6b0c07ce9c7e3992494bf8dcc818
Alex Martin <sharkfinpro@hotmail.com>
2025-12-18 16:21:12 -0800

    Add transparent color blend state to pipeline options
    Introduced a transparent color blend state in GraphicsPipelineStates and updated RectPipeline to use it via GraphicsPipelineOptions. This enables proper alpha blending for 2D rectangle rendering.

--- source/components/pipelines/implementations/2D/RectPipeline.cpp
+++ source/components/pipelines/implementations/2D/RectPipeline.cpp
@@ -1,13 +1,39 @@
 #include "RectPipeline.h"
+#include "../common/GraphicsPipelineStates.h"
 #include "../../../commandBuffer/CommandBuffer.h"
 #include "../../../renderingManager/renderer2D/Renderer2D.h"
-#include "../../../window/Window.h"
 
 namespace vke {
   RectPipeline::RectPipeline(const std::shared_ptr<LogicalDevice>& logicalDevice,
                              std::shared_ptr<RenderPass> renderPass)
     : GraphicsPipeline(logicalDevice)
   {
+    const GraphicsPipelineOptions graphicsPipelineOptions {
+      .shaders {
+        .vertexShader = "assets/shaders/2D/rect.vert.spv",
+        .fragmentShader = "assets/shaders/2D/rect.frag.spv"
+      },
+      .states {
+        .colorBlendState = GraphicsPipelineStates::colorBlendStateTransparent,
+        .depthStencilState = GraphicsPipelineStates::depthStencilStateNone,
+        .dynamicState = GraphicsPipelineStates::dynamicState,
+        .inputAssemblyState = GraphicsPipelineStates::inputAssemblyStateTriangleStrip,
+        .multisampleState = GraphicsPipelineStates::getMultsampleStateAlpha(m_logicalDevice),
+        .rasterizationState = GraphicsPipelineStates::rasterizationStateNoCull,
+        .vertexInputState = GraphicsPipelineStates::vertexInputStateRaw,
+        .viewportState = GraphicsPipelineStates::viewportState
+      },
+      .pushConstantRanges {
+        {
+          .stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
+          .offset = 0,
+          .size = sizeof(RectPushConstant)
+        }
+      },
+      .renderPass = renderPass
+    };
+
+    createPipeline(graphicsPipelineOptions);
   }
 
   void RectPipeline::render(const RenderInfo* renderInfo,
