95632561c8ce9b98f4d6a6b19c7ddca7d7832803
Alex Martin <sharkfinpro@hotmail.com>
2025-12-26 15:18:48 -0800

    Indent RenderPass class and methods under vke namespace
    Moved the RenderPass class and its method definitions to be indented within the vke namespace in both the header and source files. This improves code readability and clarifies namespace scoping.

--- source/components/renderingManager/legacyRenderer/RenderPass.h
+++ source/components/renderingManager/legacyRenderer/RenderPass.h
@@ -6,32 +6,32 @@
 
 namespace vke {
 
-class CommandBuffer;
-class LogicalDevice;
+  class CommandBuffer;
+  class LogicalDevice;
 
-class RenderPass {
-public:
-  RenderPass(const std::shared_ptr<LogicalDevice>& logicalDevice,
-             VkFormat imageFormat,
-             VkSampleCountFlagBits msaaSamples,
-             VkImageLayout finalLayout);
-  ~RenderPass();
+  class RenderPass {
+  public:
+    RenderPass(const std::shared_ptr<LogicalDevice>& logicalDevice,
+               VkFormat imageFormat,
+               VkSampleCountFlagBits msaaSamples,
+               VkImageLayout finalLayout);
+    ~RenderPass();
 
-  VkRenderPass& getRenderPass();
+    VkRenderPass& getRenderPass();
 
-  void begin(const VkFramebuffer& framebuffer,
-             const VkExtent2D& extent,
-             const std::shared_ptr<CommandBuffer>& commandBuffer) const;
+    void begin(const VkFramebuffer& framebuffer,
+               const VkExtent2D& extent,
+               const std::shared_ptr<CommandBuffer>& commandBuffer) const;
 
-private:
-  std::shared_ptr<LogicalDevice> m_logicalDevice;
+  private:
+    std::shared_ptr<LogicalDevice> m_logicalDevice;
 
-  VkRenderPass m_renderPass = VK_NULL_HANDLE;
+    VkRenderPass m_renderPass = VK_NULL_HANDLE;
 
-  void createRenderPass(VkFormat imageFormat,
-                        VkSampleCountFlagBits msaaSamples,
-                        VkImageLayout finalLayout);
-};
+    void createRenderPass(VkFormat imageFormat,
+                          VkSampleCountFlagBits msaaSamples,
+                          VkImageLayout finalLayout);
+  };
 
 } // namespace vke
 
