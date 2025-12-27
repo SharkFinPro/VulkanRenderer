c7c64563d94545adafd3f3d7c1f559ffb9fdc123
Alex Martin <sharkfinpro@hotmail.com>
2025-12-26 22:09:17 -0800

    Reformat RenderingManager for consistent indentation
    Adjusted the indentation and formatting of RenderingManager.cpp and RenderingManager.h to improve code readability and maintain consistent style. No functional changes were made.

--- source/components/renderingManager/RenderingManager.h
+++ source/components/renderingManager/RenderingManager.h
@@ -9,101 +9,101 @@
 
 namespace vke {
 
-class AssetManager;
-class CommandBuffer;
-class LightingManager;
-class LogicalDevice;
-class MousePicker;
-class PipelineManager;
-class Renderer;
-class Renderer2D;
-class SwapChain;
-class Window;
+  class AssetManager;
+  class CommandBuffer;
+  class LightingManager;
+  class LogicalDevice;
+  class MousePicker;
+  class PipelineManager;
+  class Renderer;
+  class Renderer2D;
+  class SwapChain;
+  class Window;
 
-class RenderingManager {
-public:
-  RenderingManager(const std::shared_ptr<LogicalDevice>& logicalDevice,
-                   const std::shared_ptr<Window>& window,
-                   const std::shared_ptr<MousePicker>& mousePicker,
-                   VkCommandPool commandPool,
-                   bool shouldRenderOffscreen,
-                   const char* sceneViewName,
-                   std::shared_ptr<AssetManager> assetManager);
+  class RenderingManager {
+  public:
+    RenderingManager(const std::shared_ptr<LogicalDevice>& logicalDevice,
+                     const std::shared_ptr<Window>& window,
+                     const std::shared_ptr<MousePicker>& mousePicker,
+                     VkCommandPool commandPool,
+                     bool shouldRenderOffscreen,
+                     const char* sceneViewName,
+                     std::shared_ptr<AssetManager> assetManager);
 
-  void doRendering(const std::shared_ptr<PipelineManager>& pipelineManager,
-                   const std::shared_ptr<LightingManager>& lightingManager,
-                   uint32_t currentFrame);
+    void doRendering(const std::shared_ptr<PipelineManager>& pipelineManager,
+                     const std::shared_ptr<LightingManager>& lightingManager,
+                     uint32_t currentFrame);
 
-  [[nodiscard]] std::shared_ptr<SwapChain> getSwapChain() const;
+    [[nodiscard]] std::shared_ptr<SwapChain> getSwapChain() const;
 
-  void setCameraParameters(glm::vec3 position, const glm::mat4& viewMatrix);
+    void setCameraParameters(glm::vec3 position, const glm::mat4& viewMatrix);
 
-  [[nodiscard]] std::shared_ptr<Renderer> getRenderer() const;
+    [[nodiscard]] std::shared_ptr<Renderer> getRenderer() const;
 
-  void markFramebufferResized();
+    void markFramebufferResized();
 
-  [[nodiscard]] bool isSceneFocused() const;
+    [[nodiscard]] bool isSceneFocused() const;
 
-  void recreateSwapChain();
+    void recreateSwapChain();
 
-  void enableGrid();
+    void enableGrid();
 
-  void disableGrid();
+    void disableGrid();
 
-  [[nodiscard]] bool isGridEnabled() const;
+    [[nodiscard]] bool isGridEnabled() const;
 
-  void createNewFrame() const;
+    void createNewFrame() const;
 
-  [[nodiscard]] std::shared_ptr<Renderer2D> getRenderer2D() const;
+    [[nodiscard]] std::shared_ptr<Renderer2D> getRenderer2D() const;
 
-private:
-  std::shared_ptr<LogicalDevice> m_logicalDevice;
+  private:
+    std::shared_ptr<LogicalDevice> m_logicalDevice;
 
-  std::shared_ptr<Window> m_window;
+    std::shared_ptr<Window> m_window;
 
-  std::shared_ptr<MousePicker> m_mousePicker;
+    std::shared_ptr<MousePicker> m_mousePicker;
 
-  std::shared_ptr<Renderer> m_renderer;
+    std::shared_ptr<Renderer> m_renderer;
 
-  VkCommandPool m_commandPool = VK_NULL_HANDLE;
+    VkCommandPool m_commandPool = VK_NULL_HANDLE;
 
-  std::shared_ptr<CommandBuffer> m_offscreenCommandBuffer;
-  std::shared_ptr<CommandBuffer> m_swapchainCommandBuffer;
+    std::shared_ptr<CommandBuffer> m_offscreenCommandBuffer;
+    std::shared_ptr<CommandBuffer> m_swapchainCommandBuffer;
 
-  std::shared_ptr<SwapChain> m_swapChain;
+    std::shared_ptr<SwapChain> m_swapChain;
 
-  glm::vec3 m_viewPosition{};
-  glm::mat4 m_viewMatrix{};
+    glm::vec3 m_viewPosition{};
+    glm::mat4 m_viewMatrix{};
 
-  bool m_shouldRenderOffscreen;
+    bool m_shouldRenderOffscreen;
 
-  bool m_framebufferResized = false;
+    bool m_framebufferResized = false;
 
-  bool m_sceneIsFocused = false;
+    bool m_sceneIsFocused = false;
 
-  VkExtent2D m_offscreenViewportExtent{};
+    VkExtent2D m_offscreenViewportExtent{};
 
-  ImVec2 m_offscreenViewportPos{0, 0};
+    ImVec2 m_offscreenViewportPos{0, 0};
 
-  const char* m_sceneViewName;
+    const char* m_sceneViewName;
 
-  bool m_shouldRenderGrid = true;
+    bool m_shouldRenderGrid = true;
 
-  std::shared_ptr<Renderer2D> m_renderer2D;
+    std::shared_ptr<Renderer2D> m_renderer2D;
 
-  void renderGuiScene(uint32_t imageIndex);
+    void renderGuiScene(uint32_t imageIndex);
 
-  void recordOffscreenCommandBuffer(const std::shared_ptr<PipelineManager>& pipelineManager,
-                                    const std::shared_ptr<LightingManager>& lightingManager,
-                                    uint32_t currentFrame,
-                                    uint32_t imageIndex) const;
+    void recordOffscreenCommandBuffer(const std::shared_ptr<PipelineManager>& pipelineManager,
+                                      const std::shared_ptr<LightingManager>& lightingManager,
+                                      uint32_t currentFrame,
+                                      uint32_t imageIndex) const;
 
-  void recordSwapchainCommandBuffer(const std::shared_ptr<PipelineManager>& pipelineManager, uint32_t currentFrame,
-                                    uint32_t imageIndex) const;
+    void recordSwapchainCommandBuffer(const std::shared_ptr<PipelineManager>& pipelineManager, uint32_t currentFrame,
+                                      uint32_t imageIndex) const;
 
-  static void resetDepthBuffer(VkCommandBuffer commandBuffer,
-                               VkExtent2D extent);
-};
+    static void resetDepthBuffer(VkCommandBuffer commandBuffer,
+                                 VkExtent2D extent);
+  };
 
 } // namespace vke
 
