ed96605c81bcfc4331097b73f70b2e9c0e9e6f68
Alex Martin <sharkfinpro@hotmail.com>
2025-12-26 22:21:30 -0800

    Reformat PhysicalDevice class with consistent indentation
    Refactored PhysicalDevice.cpp and PhysicalDevice.h to use consistent indentation and code style, improving readability and maintainability. No functional changes were made.

--- source/components/physicalDevice/PhysicalDevice.h
+++ source/components/physicalDevice/PhysicalDevice.h
@@ -9,91 +9,91 @@
 
 namespace vke {
 
-class Instance;
+  class Instance;
 
-#ifdef __APPLE__
-constexpr std::array<const char*, 3> deviceExtensions {
-  VK_KHR_SWAPCHAIN_EXTENSION_NAME,
-  VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME
-  "VK_KHR_portability_subset"
-};
-#else
-constexpr std::array<const char*, 2> deviceExtensions {
-  VK_KHR_SWAPCHAIN_EXTENSION_NAME,
-  VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME
-};
-#endif
+  #ifdef __APPLE__
+  constexpr std::array<const char*, 3> deviceExtensions {
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+    VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME
+    "VK_KHR_portability_subset"
+  };
+  #else
+  constexpr std::array<const char*, 2> deviceExtensions {
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+    VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME
+  };
+  #endif
 
-struct QueueFamilyIndices {
-  std::optional<uint32_t> graphicsFamily;
-  std::optional<uint32_t> presentFamily;
-  std::optional<uint32_t> computeFamily;
+  struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+    std::optional<uint32_t> presentFamily;
+    std::optional<uint32_t> computeFamily;
 
-  [[nodiscard]] bool isComplete() const
-  {
-    return graphicsFamily.has_value() &&
-           presentFamily.has_value() &&
-           computeFamily.has_value();
-  }
-};
+    [[nodiscard]] bool isComplete() const
+    {
+      return graphicsFamily.has_value() &&
+             presentFamily.has_value() &&
+             computeFamily.has_value();
+    }
+  };
 
-struct SwapChainSupportDetails {
-  VkSurfaceCapabilitiesKHR capabilities {};
-  std::vector<VkSurfaceFormatKHR> formats;
-  std::vector<VkPresentModeKHR> presentModes;
-};
+  struct SwapChainSupportDetails {
+    VkSurfaceCapabilitiesKHR capabilities {};
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
+  };
 
-class PhysicalDevice {
-public:
-  PhysicalDevice(const std::shared_ptr<Instance>& instance, VkSurfaceKHR& surface);
+  class PhysicalDevice {
+  public:
+    PhysicalDevice(const std::shared_ptr<Instance>& instance, VkSurfaceKHR& surface);
 
-  [[nodiscard]] QueueFamilyIndices getQueueFamilies() const;
+    [[nodiscard]] QueueFamilyIndices getQueueFamilies() const;
 
-  [[nodiscard]] SwapChainSupportDetails getSwapChainSupport() const;
+    [[nodiscard]] SwapChainSupportDetails getSwapChainSupport() const;
 
-  [[nodiscard]] VkSampleCountFlagBits getMsaaSamples() const;
+    [[nodiscard]] VkSampleCountFlagBits getMsaaSamples() const;
 
-  [[nodiscard]] uint32_t findMemoryType(uint32_t typeFilter, const VkMemoryPropertyFlags& properties) const;
+    [[nodiscard]] uint32_t findMemoryType(uint32_t typeFilter, const VkMemoryPropertyFlags& properties) const;
 
-  void updateSwapChainSupportDetails();
+    void updateSwapChainSupportDetails();
 
-  [[nodiscard]] VkFormatProperties getFormatProperties(VkFormat format) const;
+    [[nodiscard]] VkFormatProperties getFormatProperties(VkFormat format) const;
 
-  [[nodiscard]] VkPhysicalDeviceProperties getDeviceProperties() const;
+    [[nodiscard]] VkPhysicalDeviceProperties getDeviceProperties() const;
 
-  [[nodiscard]] VkDevice createLogicalDevice(const VkDeviceCreateInfo& deviceCreateInfo) const;
+    [[nodiscard]] VkDevice createLogicalDevice(const VkDeviceCreateInfo& deviceCreateInfo) const;
 
-  [[nodiscard]] VkFormat findDepthFormat() const;
+    [[nodiscard]] VkFormat findDepthFormat() const;
 
-  [[nodiscard]] VkFormat findSupportedFormat(const std::vector<VkFormat>& candidates,
-                                             VkImageTiling tiling,
-                                             VkFormatFeatureFlags features) const;
+    [[nodiscard]] VkFormat findSupportedFormat(const std::vector<VkFormat>& candidates,
+                                               VkImageTiling tiling,
+                                               VkFormatFeatureFlags features) const;
 
-  friend class ImGuiInstance;
+    friend class ImGuiInstance;
 
-private:
-  VkPhysicalDevice m_physicalDevice = VK_NULL_HANDLE;
+  private:
+    VkPhysicalDevice m_physicalDevice = VK_NULL_HANDLE;
 
-  VkSurfaceKHR& m_surface;
+    VkSurfaceKHR& m_surface;
 
-  VkSampleCountFlagBits m_msaaSamples;
+    VkSampleCountFlagBits m_msaaSamples;
 
-  QueueFamilyIndices m_queueFamilyIndices;
+    QueueFamilyIndices m_queueFamilyIndices;
 
-  SwapChainSupportDetails m_swapChainSupportDetails;
+    SwapChainSupportDetails m_swapChainSupportDetails;
 
-  void pickPhysicalDevice(const std::shared_ptr<Instance>& instance);
+    void pickPhysicalDevice(const std::shared_ptr<Instance>& instance);
 
-  bool isDeviceSuitable(VkPhysicalDevice device) const;
+    bool isDeviceSuitable(VkPhysicalDevice device) const;
 
-  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) const;
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) const;
 
-  static bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+    static bool checkDeviceExtensionSupport(VkPhysicalDevice device);
 
-  SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device) const;
+    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device) const;
 
-  [[nodiscard]] VkSampleCountFlagBits getMaxUsableSampleCount() const;
-};
+    [[nodiscard]] VkSampleCountFlagBits getMaxUsableSampleCount() const;
+  };
 
 } // namespace vke
 
