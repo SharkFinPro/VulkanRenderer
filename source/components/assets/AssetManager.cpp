4d0e7c564d94be6cf642aea1446c1e4112205815
Alex Martin <sharkfinpro@hotmail.com>
2025-12-26 22:28:27 -0800

    Refactor AssetManager to use consistent indentation and scoping
    Updated AssetManager.cpp and AssetManager.h to use consistent indentation and namespace scoping. All member functions are now defined within the vke namespace using explicit scoping, improving code readability and maintainability. No functional changes were made.

--- source/components/assets/AssetManager.cpp
+++ source/components/assets/AssetManager.cpp
@@ -9,177 +9,178 @@
 
 namespace vke {
 
-AssetManager::AssetManager(const std::shared_ptr<LogicalDevice>& logicalDevice,
-                           VkCommandPool commandPool,
-                           VkDescriptorPool descriptorPool)
-  : m_logicalDevice(logicalDevice), m_commandPool(commandPool), m_descriptorPool(descriptorPool)
-{
-  createDescriptorSetLayouts();
-}
-
-AssetManager::~AssetManager()
-{
-  m_logicalDevice->destroyDescriptorSetLayout(m_objectDescriptorSetLayout);
-
-  m_logicalDevice->destroyDescriptorSetLayout(m_fontDescriptorSetLayout);
-}
-
-std::shared_ptr<Texture2D> AssetManager::loadTexture(const char* path, bool repeat)
-{
-  auto texture = std::make_shared<Texture2D>(m_logicalDevice, m_commandPool, path,
-                                             repeat ? VK_SAMPLER_ADDRESS_MODE_REPEAT : VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE);
-  m_textures.push_back(texture);
-
-  return texture;
-}
-
-std::shared_ptr<Model> AssetManager::loadModel(const char* path, glm::vec3 rotation)
-{
-  auto model = std::make_shared<Model>(
-    m_logicalDevice,
-    m_commandPool,
-    path,
-    rotation
-  );
-
-  m_models.push_back(model);
-
-  return model;
-}
-
-std::shared_ptr<RenderObject> AssetManager::loadRenderObject(const std::shared_ptr<Texture2D>& texture,
-                                                             const std::shared_ptr<Texture2D>& specularMap,
-                                                             const std::shared_ptr<Model>& model)
-{
-  auto renderObject = std::make_shared<RenderObject>(
-    m_logicalDevice,
-    m_objectDescriptorSetLayout,
-    texture,
-    specularMap,
-    model
-  );
-
-  m_renderObjects.push_back(renderObject);
-
-  return renderObject;
-
-}
-
-void AssetManager::registerFont(std::string fontName, std::string fontPath)
-{
-  m_fontNames.insert({fontName, fontPath});
-}
-
-std::shared_ptr<Font> AssetManager::getFont(const std::string& fontName, const uint32_t fontSize)
-{
-  auto fontByName = m_fonts.find(fontName);
-
-  if (fontByName == m_fonts.end())
+  AssetManager::AssetManager(const std::shared_ptr<LogicalDevice>& logicalDevice,
+                             VkCommandPool commandPool,
+                             VkDescriptorPool descriptorPool)
+    : m_logicalDevice(logicalDevice), m_commandPool(commandPool), m_descriptorPool(descriptorPool)
   {
-    loadFont(fontName, fontSize);
-
-    fontByName = m_fonts.find(fontName);
+    createDescriptorSetLayouts();
   }
 
-  auto fontBySize = fontByName->second.find(fontSize);
-  if (fontBySize == fontByName->second.end())
+  AssetManager::~AssetManager()
   {
-    loadFont(fontName, fontSize);
+    m_logicalDevice->destroyDescriptorSetLayout(m_objectDescriptorSetLayout);
 
-    fontBySize = fontByName->second.find(fontSize);
+    m_logicalDevice->destroyDescriptorSetLayout(m_fontDescriptorSetLayout);
   }
 
-  return fontBySize->second;
-}
-
-VkDescriptorSetLayout AssetManager::getObjectDescriptorSetLayout() const
-{
-  return m_objectDescriptorSetLayout;
-}
-
-VkDescriptorSetLayout AssetManager::getFontDescriptorSetLayout() const
-{
-  return m_fontDescriptorSetLayout;
-}
-
-void AssetManager::createDescriptorSetLayouts()
-{
-  createObjectDescriptorSetLayout();
-
-  createFontDescriptorSetLayout();
-}
-
-void AssetManager::createObjectDescriptorSetLayout()
-{
-  constexpr VkDescriptorSetLayoutBinding transformLayout {
-    .binding = 0,
-    .descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
-    .descriptorCount = 1,
-    .stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_GEOMETRY_BIT | VK_SHADER_STAGE_FRAGMENT_BIT
-  };
-
-  constexpr VkDescriptorSetLayoutBinding textureLayout {
-    .binding = 1,
-    .descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-    .descriptorCount = 1,
-    .stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT
-  };
-
-  constexpr VkDescriptorSetLayoutBinding specularLayout {
-    .binding = 4,
-    .descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-    .descriptorCount = 1,
-    .stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT
-  };
-
-  constexpr std::array objectBindings {
-    transformLayout,
-    textureLayout,
-    specularLayout
-  };
-
-  const VkDescriptorSetLayoutCreateInfo objectLayoutCreateInfo {
-    .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
-    .bindingCount = static_cast<uint32_t>(objectBindings.size()),
-    .pBindings = objectBindings.data()
-  };
-
-  m_objectDescriptorSetLayout = m_logicalDevice->createDescriptorSetLayout(objectLayoutCreateInfo);
-}
-
-void AssetManager::createFontDescriptorSetLayout()
-{
-  constexpr VkDescriptorSetLayoutBinding glyphDescriptorSetLayoutBinding {
-    .binding = 0,
-    .descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-    .descriptorCount = 1,
-    .stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT
-  };
-
-  std::vector<VkDescriptorSetLayoutBinding> descriptorSetLayoutBindings {
-    glyphDescriptorSetLayoutBinding
-  };
-
-  const VkDescriptorSetLayoutCreateInfo descriptorSetLayoutCreateInfo {
-    .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
-    .bindingCount = static_cast<uint32_t>(descriptorSetLayoutBindings.size()),
-    .pBindings = descriptorSetLayoutBindings.data()
-  };
-
-  m_fontDescriptorSetLayout = m_logicalDevice->createDescriptorSetLayout(descriptorSetLayoutCreateInfo);
-}
-
-void AssetManager::loadFont(const std::string& fontName, uint32_t fontSize)
-{
-  const auto fontPath = m_fontNames.find(fontName);
-
-  if (fontPath == m_fontNames.end())
+  std::shared_ptr<Texture2D> AssetManager::loadTexture(const char* path, bool repeat)
   {
-    throw std::runtime_error("Font not found!");
+    auto texture = std::make_shared<Texture2D>(m_logicalDevice, m_commandPool, path,
+                                               repeat ? VK_SAMPLER_ADDRESS_MODE_REPEAT : VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE);
+    m_textures.push_back(texture);
+
+    return texture;
   }
 
-  auto font = std::make_shared<Font>(m_logicalDevice, fontPath->second, fontSize, m_commandPool, m_descriptorPool, m_fontDescriptorSetLayout);
+  std::shared_ptr<Model> AssetManager::loadModel(const char* path, glm::vec3 rotation)
+  {
+    auto model = std::make_shared<Model>(
+      m_logicalDevice,
+      m_commandPool,
+      path,
+      rotation
+    );
 
-  m_fonts[fontName].insert({fontSize, font});
-}
+    m_models.push_back(model);
+
+    return model;
+  }
+
+  std::shared_ptr<RenderObject> AssetManager::loadRenderObject(const std::shared_ptr<Texture2D>& texture,
+                                                               const std::shared_ptr<Texture2D>& specularMap,
+                                                               const std::shared_ptr<Model>& model)
+  {
+    auto renderObject = std::make_shared<RenderObject>(
+      m_logicalDevice,
+      m_objectDescriptorSetLayout,
+      texture,
+      specularMap,
+      model
+    );
+
+    m_renderObjects.push_back(renderObject);
+
+    return renderObject;
+
+  }
+
+  void AssetManager::registerFont(std::string fontName, std::string fontPath)
+  {
+    m_fontNames.insert({fontName, fontPath});
+  }
+
+  std::shared_ptr<Font> AssetManager::getFont(const std::string& fontName, const uint32_t fontSize)
+  {
+    auto fontByName = m_fonts.find(fontName);
+
+    if (fontByName == m_fonts.end())
+    {
+      loadFont(fontName, fontSize);
+
+      fontByName = m_fonts.find(fontName);
+    }
+
+    auto fontBySize = fontByName->second.find(fontSize);
+    if (fontBySize == fontByName->second.end())
+    {
+      loadFont(fontName, fontSize);
+
+      fontBySize = fontByName->second.find(fontSize);
+    }
+
+    return fontBySize->second;
+  }
+
+  VkDescriptorSetLayout AssetManager::getObjectDescriptorSetLayout() const
+  {
+    return m_objectDescriptorSetLayout;
+  }
+
+  VkDescriptorSetLayout AssetManager::getFontDescriptorSetLayout() const
+  {
+    return m_fontDescriptorSetLayout;
+  }
+
+  void AssetManager::createDescriptorSetLayouts()
+  {
+    createObjectDescriptorSetLayout();
+
+    createFontDescriptorSetLayout();
+  }
+
+  void AssetManager::createObjectDescriptorSetLayout()
+  {
+    constexpr VkDescriptorSetLayoutBinding transformLayout {
+      .binding = 0,
+      .descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
+      .descriptorCount = 1,
+      .stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_GEOMETRY_BIT | VK_SHADER_STAGE_FRAGMENT_BIT
+    };
+
+    constexpr VkDescriptorSetLayoutBinding textureLayout {
+      .binding = 1,
+      .descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
+      .descriptorCount = 1,
+      .stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT
+    };
+
+    constexpr VkDescriptorSetLayoutBinding specularLayout {
+      .binding = 4,
+      .descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
+      .descriptorCount = 1,
+      .stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT
+    };
+
+    constexpr std::array objectBindings {
+      transformLayout,
+      textureLayout,
+      specularLayout
+    };
+
+    const VkDescriptorSetLayoutCreateInfo objectLayoutCreateInfo {
+      .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
+      .bindingCount = static_cast<uint32_t>(objectBindings.size()),
+      .pBindings = objectBindings.data()
+    };
+
+    m_objectDescriptorSetLayout = m_logicalDevice->createDescriptorSetLayout(objectLayoutCreateInfo);
+  }
+
+  void AssetManager::createFontDescriptorSetLayout()
+  {
+    constexpr VkDescriptorSetLayoutBinding glyphDescriptorSetLayoutBinding {
+      .binding = 0,
+      .descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
+      .descriptorCount = 1,
+      .stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT
+    };
+
+    std::vector<VkDescriptorSetLayoutBinding> descriptorSetLayoutBindings {
+      glyphDescriptorSetLayoutBinding
+    };
+
+    const VkDescriptorSetLayoutCreateInfo descriptorSetLayoutCreateInfo {
+      .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
+      .bindingCount = static_cast<uint32_t>(descriptorSetLayoutBindings.size()),
+      .pBindings = descriptorSetLayoutBindings.data()
+    };
+
+    m_fontDescriptorSetLayout = m_logicalDevice->createDescriptorSetLayout(descriptorSetLayoutCreateInfo);
+  }
+
+  void AssetManager::loadFont(const std::string& fontName, uint32_t fontSize)
+  {
+    const auto fontPath = m_fontNames.find(fontName);
+
+    if (fontPath == m_fontNames.end())
+    {
+      throw std::runtime_error("Font not found!");
+    }
+
+    auto font = std::make_shared<Font>(m_logicalDevice, fontPath->second, fontSize, m_commandPool, m_descriptorPool, m_fontDescriptorSetLayout);
+
+    m_fonts[fontName].insert({fontSize, font});
+  }
+
 } // namespace vke
--- source/components/assets/AssetManager.h
+++ source/components/assets/AssetManager.h
@@ -10,63 +10,63 @@
 
 namespace vke {
 
-class Font;
-class LogicalDevice;
-class Model;
-class RenderObject;
-class Texture;
-class Texture2D;
+  class Font;
+  class LogicalDevice;
+  class Model;
+  class RenderObject;
+  class Texture;
+  class Texture2D;
 
-class AssetManager {
-public:
-  AssetManager(const std::shared_ptr<LogicalDevice>& logicalDevice,
-               VkCommandPool commandPool,
-               VkDescriptorPool descriptorPool);
+  class AssetManager {
+  public:
+    AssetManager(const std::shared_ptr<LogicalDevice>& logicalDevice,
+                 VkCommandPool commandPool,
+                 VkDescriptorPool descriptorPool);
 
-  ~AssetManager();
+    ~AssetManager();
 
-  [[nodiscard]] std::shared_ptr<Texture2D> loadTexture(const char* path, bool repeat = true);
+    [[nodiscard]] std::shared_ptr<Texture2D> loadTexture(const char* path, bool repeat = true);
 
-  [[nodiscard]] std::shared_ptr<Model> loadModel(const char* path, glm::vec3 rotation = { 0, 0, 0 });
+    [[nodiscard]] std::shared_ptr<Model> loadModel(const char* path, glm::vec3 rotation = { 0, 0, 0 });
 
-  [[nodiscard]] std::shared_ptr<RenderObject> loadRenderObject(const std::shared_ptr<Texture2D>& texture,
-                                                               const std::shared_ptr<Texture2D>& specularMap,
-                                                               const std::shared_ptr<Model>& model);
+    [[nodiscard]] std::shared_ptr<RenderObject> loadRenderObject(const std::shared_ptr<Texture2D>& texture,
+                                                                 const std::shared_ptr<Texture2D>& specularMap,
+                                                                 const std::shared_ptr<Model>& model);
 
-  void registerFont(std::string fontName, std::string fontPath);
+    void registerFont(std::string fontName, std::string fontPath);
 
-  [[nodiscard]] std::shared_ptr<Font> getFont(const std::string& fontName, uint32_t fontSize);
+    [[nodiscard]] std::shared_ptr<Font> getFont(const std::string& fontName, uint32_t fontSize);
 
-  [[nodiscard]] VkDescriptorSetLayout getObjectDescriptorSetLayout() const;
+    [[nodiscard]] VkDescriptorSetLayout getObjectDescriptorSetLayout() const;
 
-  [[nodiscard]] VkDescriptorSetLayout getFontDescriptorSetLayout() const;
+    [[nodiscard]] VkDescriptorSetLayout getFontDescriptorSetLayout() const;
 
-private:
-  std::shared_ptr<LogicalDevice> m_logicalDevice;
+  private:
+    std::shared_ptr<LogicalDevice> m_logicalDevice;
 
-  VkCommandPool m_commandPool = VK_NULL_HANDLE;
+    VkCommandPool m_commandPool = VK_NULL_HANDLE;
 
-  VkDescriptorSetLayout m_objectDescriptorSetLayout = VK_NULL_HANDLE;
+    VkDescriptorSetLayout m_objectDescriptorSetLayout = VK_NULL_HANDLE;
 
-  VkDescriptorSetLayout m_fontDescriptorSetLayout = VK_NULL_HANDLE;
+    VkDescriptorSetLayout m_fontDescriptorSetLayout = VK_NULL_HANDLE;
 
-  VkDescriptorPool m_descriptorPool = VK_NULL_HANDLE;
+    VkDescriptorPool m_descriptorPool = VK_NULL_HANDLE;
 
-  std::vector<std::shared_ptr<Texture>> m_textures;
-  std::vector<std::shared_ptr<Model>> m_models;
-  std::vector<std::shared_ptr<RenderObject>> m_renderObjects;
+    std::vector<std::shared_ptr<Texture>> m_textures;
+    std::vector<std::shared_ptr<Model>> m_models;
+    std::vector<std::shared_ptr<RenderObject>> m_renderObjects;
 
-  std::unordered_map<std::string, std::string> m_fontNames;
-  std::unordered_map<std::string, std::unordered_map<uint32_t, std::shared_ptr<Font>>> m_fonts;
+    std::unordered_map<std::string, std::string> m_fontNames;
+    std::unordered_map<std::string, std::unordered_map<uint32_t, std::shared_ptr<Font>>> m_fonts;
 
-  void createDescriptorSetLayouts();
+    void createDescriptorSetLayouts();
 
-  void createObjectDescriptorSetLayout();
+    void createObjectDescriptorSetLayout();
 
-  void createFontDescriptorSetLayout();
+    void createFontDescriptorSetLayout();
 
-  void loadFont(const std::string& fontName, uint32_t fontSize);
-};
+    void loadFont(const std::string& fontName, uint32_t fontSize);
+  };
 
 } // namespace vke
 
