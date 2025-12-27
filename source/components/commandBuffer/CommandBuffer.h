ee71635418ff4b95380787cbbb7e20457f29a7a1
Alex Martin <sharkfinpro@hotmail.com>
2025-12-26 22:27:39 -0800

    Indent CommandBuffer class under vke namespace
    Moved the CommandBuffer class and its implementation into an indented block within the vke namespace for improved code organization and readability. No functional changes were made.

--- source/components/commandBuffer/CommandBuffer.h
+++ source/components/commandBuffer/CommandBuffer.h
@@ -8,70 +8,70 @@
 
 namespace vke {
 
-class LogicalDevice;
+  class LogicalDevice;
 
-class CommandBuffer {
-public:
-  CommandBuffer(const std::shared_ptr<LogicalDevice>& logicalDevice, VkCommandPool commandPool);
+  class CommandBuffer {
+  public:
+    CommandBuffer(const std::shared_ptr<LogicalDevice>& logicalDevice, VkCommandPool commandPool);
 
-  void setCurrentFrame(uint32_t currentFrame);
+    void setCurrentFrame(uint32_t currentFrame);
 
-  void record(const std::function<void()>& renderFunction) const;
+    void record(const std::function<void()>& renderFunction) const;
 
-  void resetCommandBuffer() const;
+    void resetCommandBuffer() const;
 
-  [[nodiscard]] VkCommandBuffer* getCommandBuffer();
+    [[nodiscard]] VkCommandBuffer* getCommandBuffer();
 
-  void setViewport(const VkViewport& viewport) const;
+    void setViewport(const VkViewport& viewport) const;
 
-  void setScissor(const VkRect2D& scissor) const;
+    void setScissor(const VkRect2D& scissor) const;
 
-  void beginRenderPass(const VkRenderPassBeginInfo& renderPassBeginInfo) const;
+    void beginRenderPass(const VkRenderPassBeginInfo& renderPassBeginInfo) const;
 
-  void endRenderPass() const;
+    void endRenderPass() const;
 
-  void beginRendering(const VkRenderingInfo& renderingInfo) const;
+    void beginRendering(const VkRenderingInfo& renderingInfo) const;
 
-  void endRendering() const;
+    void endRendering() const;
 
-  void bindPipeline(VkPipelineBindPoint pipelineBindPoint, VkPipeline pipeline) const;
+    void bindPipeline(VkPipelineBindPoint pipelineBindPoint, VkPipeline pipeline) const;
 
-  void bindDescriptorSets(VkPipelineBindPoint pipelineBindPoint, VkPipelineLayout pipelineLayout, uint32_t firstSet,
-                          uint32_t descriptorSetCount, const VkDescriptorSet* descriptorSets) const;
+    void bindDescriptorSets(VkPipelineBindPoint pipelineBindPoint, VkPipelineLayout pipelineLayout, uint32_t firstSet,
+                            uint32_t descriptorSetCount, const VkDescriptorSet* descriptorSets) const;
 
-  void dispatch(uint32_t groupCountX, uint32_t groupCountY, uint32_t groupCountZ) const;
+    void dispatch(uint32_t groupCountX, uint32_t groupCountY, uint32_t groupCountZ) const;
 
-  void bindVertexBuffers(uint32_t firstBinding, uint32_t bindingCount, const VkBuffer* buffers,
-                         const VkDeviceSize* offsets) const;
+    void bindVertexBuffers(uint32_t firstBinding, uint32_t bindingCount, const VkBuffer* buffers,
+                           const VkDeviceSize* offsets) const;
 
-  void bindIndexBuffer(VkBuffer buffer, VkDeviceSize offset, VkIndexType indexType) const;
+    void bindIndexBuffer(VkBuffer buffer, VkDeviceSize offset, VkIndexType indexType) const;
 
-  void draw(uint32_t vertexCount, uint32_t instanceCount, uint32_t firstVertex, uint32_t firstInstance) const;
+    void draw(uint32_t vertexCount, uint32_t instanceCount, uint32_t firstVertex, uint32_t firstInstance) const;
 
-  void drawIndexed(uint32_t indexCount, uint32_t instanceCount, uint32_t firstIndex, int32_t vertexOffset,
-                   uint32_t firstInstance) const;
+    void drawIndexed(uint32_t indexCount, uint32_t instanceCount, uint32_t firstIndex, int32_t vertexOffset,
+                     uint32_t firstInstance) const;
 
-  void pushConstants(VkPipelineLayout layout, VkShaderStageFlags stageFlags, uint32_t offset, uint32_t size,
-                     const void* values) const;
+    void pushConstants(VkPipelineLayout layout, VkShaderStageFlags stageFlags, uint32_t offset, uint32_t size,
+                       const void* values) const;
 
-  void pipelineBarrier(VkPipelineStageFlags srcStageMask,
-                       VkPipelineStageFlags dstStageMask,
-                       VkDependencyFlags dependencyFlags,
-                       const std::vector<VkMemoryBarrier>& memoryBarriers,
-                       const std::vector<VkBufferMemoryBarrier>& bufferMemoryBarriers,
-                       const std::vector<VkImageMemoryBarrier>& imageMemoryBarriers) const;
+    void pipelineBarrier(VkPipelineStageFlags srcStageMask,
+                         VkPipelineStageFlags dstStageMask,
+                         VkDependencyFlags dependencyFlags,
+                         const std::vector<VkMemoryBarrier>& memoryBarriers,
+                         const std::vector<VkBufferMemoryBarrier>& bufferMemoryBarriers,
+                         const std::vector<VkImageMemoryBarrier>& imageMemoryBarriers) const;
 
-  friend class ImGuiInstance;
+    friend class ImGuiInstance;
 
-private:
-  std::shared_ptr<LogicalDevice> m_logicalDevice;
+  private:
+    std::shared_ptr<LogicalDevice> m_logicalDevice;
 
-  std::vector<VkCommandBuffer> m_commandBuffers;
+    std::vector<VkCommandBuffer> m_commandBuffers;
 
-  uint32_t m_currentFrame = 0;
+    uint32_t m_currentFrame = 0;
 
-  void allocateCommandBuffers(VkCommandPool commandPool);
-};
+    void allocateCommandBuffers(VkCommandPool commandPool);
+  };
 
 } // namespace vke
 
