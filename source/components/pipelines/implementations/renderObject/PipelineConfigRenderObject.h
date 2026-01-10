d39a2e23d1f14d41b84ad4ec142d36f3f3d3f84c
Alex Martin <sharkfinpro@hotmail.com>
2026-01-10 15:11:42 -0800

    Add PipelineConfigRenderObject header file
    Introduces PipelineConfigRenderObject.h with initial includes and namespace declaration for future pipeline configuration implementations.

---
+++ source/components/pipelines/implementations/renderObject/PipelineConfigRenderObject.h
@@ -0,0 +1,13 @@
+#ifndef VULKANPROJECT_PIPELINECONFIGRENDEROBJECT_H
+#define VULKANPROJECT_PIPELINECONFIGRENDEROBJECT_H
+
+#include "../common/GraphicsPipelineStates.h"
+#include "../../GraphicsPipeline.h"
+
+namespace vke::PipelineConfig {
+
+  
+
+}
+
+#endif //VULKANPROJECT_PIPELINECONFIGRENDEROBJECT_H
