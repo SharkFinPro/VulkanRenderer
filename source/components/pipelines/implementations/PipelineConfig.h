e044f7ad7913def7318e2c5f5ba7ebd6007c4bb1
Alex Martin <sharkfinpro@hotmail.com>
2026-01-10 14:40:40 -0800

    Add PipelineConfig header file
    Introduces the PipelineConfig.h header within the vke::PipelineConfig namespace. This sets up the structure for future pipeline configuration implementations.

---
+++ source/components/pipelines/implementations/PipelineConfig.h
@@ -0,0 +1,10 @@
+#ifndef VULKANPROJECT_PIPELINECONFIG_H
+#define VULKANPROJECT_PIPELINECONFIG_H
+
+namespace vke::PipelineConfig {
+
+
+
+} // vke::PipelineConfig
+
+#endif //VULKANPROJECT_PIPELINECONFIG_H
