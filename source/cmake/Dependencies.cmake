a01ee99ffdac46204105bfd42ba36513ca0fd339
Alex Martin <sharkfinpro@hotmail.com>
2026-04-05 11:53:16 -0700

    Scope Vulkan.hpp defines to library target
    Move Vulkan.hpp preprocessor definitions from global add_compile_definitions in Dependencies.cmake into target_compile_definitions on the main project library (PUBLIC). This scopes VULKAN_HPP_NO_STRUCT_CONSTRUCTORS, VULKAN_HPP_DISPATCH_LOADER_DYNAMIC and VULKAN_HPP_HANDLE_ERROR_OUT_OF_DATE_AS_SUCCESS to the library target instead of applying them globally, reducing accidental contamination of other targets and ensuring consumers inherit the defines as intended.

--- source/cmake/Dependencies.cmake
+++ source/cmake/Dependencies.cmake
@@ -1,12 +1,6 @@
 # Vulkan
 find_package(Vulkan REQUIRED)
 
-add_compile_definitions(
-  VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
-  VULKAN_HPP_DISPATCH_LOADER_DYNAMIC
-  VULKAN_HPP_HANDLE_ERROR_OUT_OF_DATE_AS_SUCCESS
-)
-
 # Configure and fetch third-party libraries
 include(cmake/External.cmake)
 
