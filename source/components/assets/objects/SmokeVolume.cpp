c37eefaace34e7de0581a54682ad2b0961754f1b
Alex Martin <sharkfinpro@hotmail.com>
2026-04-20 15:06:26 -0700

    Inherit SmokeVolume from ProceduralVolume
    Make SmokeVolume a subclass of ProceduralVolume: add ProceduralVolume include, update the class declaration to inherit from ProceduralVolume and declare a constructor, and implement the constructor in the .cpp to forward the LogicalDevice and CommandPool to the base class.

--- source/components/assets/objects/SmokeVolume.cpp
+++ source/components/assets/objects/SmokeVolume.cpp
@@ -1,4 +1,10 @@
 #include "SmokeVolume.h"
 
 namespace vke {
+
+  SmokeVolume::SmokeVolume(std::shared_ptr<LogicalDevice> logicalDevice,
+                           const vk::CommandPool& commandPool)
+    : ProceduralVolume(std::move(logicalDevice), commandPool)
+  {}
+
 } // vke
--- source/components/assets/objects/SmokeVolume.h
+++ source/components/assets/objects/SmokeVolume.h
@@ -1,11 +1,16 @@
 #ifndef VULKANPROJECT_SMOKEVOLUME_H
 #define VULKANPROJECT_SMOKEVOLUME_H
 
+#include "ProceduralVolume.h"
+
+
 namespace vke {
 
-class SmokeVolume {
-
-};
+  class SmokeVolume : public ProceduralVolume {
+  public:
+    SmokeVolume(std::shared_ptr<LogicalDevice> logicalDevice,
+                const vk::CommandPool& commandPool);
+  };
 
 } // vke
 
