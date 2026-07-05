d1cba7f2b4b7e260ee1ed7e298c7f5b88c50cb73
Alex Martin <sharkfinpro@hotmail.com>
2026-07-05 16:57:07 -0700

    Raise shader target to Vulkan 1.3
    Update shader compilation to target Vulkan 1.3, and refresh AGENTS.md with the current FrameScheduler/synchronization overview.

--- source/cmake/CompileShaders.cmake
+++ source/cmake/CompileShaders.cmake
@@ -42,7 +42,7 @@ function(compile_shaders)
     # Add compilation command
     add_custom_command(
       OUTPUT ${SPV_FILE}
-      COMMAND glslangValidator --target-env vulkan1.2 -V -I${shadersSrc}/include ${SHADER} -o ${SPV_FILE}
+      COMMAND glslangValidator --target-env vulkan1.3 -V -I${shadersSrc}/include ${SHADER} -o ${SPV_FILE}
       DEPENDS ${SHADER} ${COMMON_SHADER_FILES}
       COMMENT "Compiling shader: ${REL_PATH}"
     )
